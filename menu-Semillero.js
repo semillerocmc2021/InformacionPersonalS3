34161d02ee53bc96741abe44807daa24b339224a
TonyDelux <tonyrpm65@gmail.com>
2021-08-10 11:40:32 -0500

    Menu agregado

--- menu-Semillero.js
+++ menu-Semillero.js
@@ -7,14 +7,15 @@
             "MNU_KUG_EVP":"Prueba Final Kevin Ulquiango", 
             "MNU_KOU_CREATCLIE":"Crear Cliente1",
             "MNU_KUG_EVAL":"Evaluacion2 Kevin Ulquiango",
-            "MNU_SMCMC":"Formulario Semiller",
+            "MNU_SMCMC":"Formulario Semillero",
             "MNU_KUG_CALC":"Calculadora Kevin Ulquiango",
             "MNU_KUG_PROM":"Calificaciones Kevin Ulquiango",
             "MNU_KOU_BVDA":"Bienvenido Kevin Ulquiango",
             "MNU_KOUG_PRU":"Formularios",
             "MNU_KUG_EVP":"Evaluacion Kevin Ulquiango",
             "MNU_KOUG_PRUCLS":"Consultar Cliente",
-            "MNU_KOUG_PRUBU":"Lista Productos"
+            "MNU_KOUG_PRUBU":"Lista Productos",
+            "MNU_EVAL_TONY":"Evaluaci\u00F3n - Tony",
         }
     }
 }
