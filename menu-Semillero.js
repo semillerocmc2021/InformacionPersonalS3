dfde8012bbf4ff1f97948059172b78f1db897dea
jonathan borbor <jonathanbor2020@gmail.com>
2021-08-10 11:43:31 -0500

    insercion menus de jonathan borbor

--- menu-Semillero.js
+++ menu-Semillero.js
@@ -14,7 +14,22 @@
             "MNU_KOUG_PRU":"Formularios",
             "MNU_KUG_EVP":"Evaluacion Kevin Ulquiango",
             "MNU_KOUG_PRUCLS":"Consultar Cliente",
-            "MNU_KOUG_PRUBU":"Lista Productos"
+            "MNU_KOUG_PRUBU":"Lista Productos",
+			
+			"MNU_JIBY_BVDA":"Bienvenido Jonathan Borbor",
+			"MNU_JIBY_CALIFICACIONES":"CALIFICACIONES Jonathan Borbor",
+			"MNU_JIBY_CALCULADORA":"CALCULADORA Jonathan Borbor",
+			"MNU_JIBY_EVANOTAS":"EVAL NOTAS Jonathan Borbor",
+			"MNU_JIBY_EVABLITIENDA":"EVAL BLIS TIENDA Jonathan Borbor",
+			"MNU_JIBY_CLIENTEFRM":"BLIS INGRESO CLIENTE Jonathan Borbor",
+			"MNU_JIBY_CONSUL_CLIENTE":"BLIS CONSULT CLIENTE Jonathan Borbor",
+			"MNU_JIBY_LISTAR_CLIENTES":"GRID LISTAR CLIENTES Jonathan Borbor",
+			"MNU_JIBY_EVA_LISTAR_TIENDAS":"GRID LISTAR TIENDAS Jonathan Borbor",
+			"JONATHAN_BORBOR":"JONATHAN BORBOR",
+			"MNU_JIBY_FORMPRODUCTO":"FORMULARIO PRODUCTO Jonathan Borbor",
+			"MNU_JIBY_LISTARPRODUCTOS":"LISTAR PRODUCTO Jonathan Borbor",
+			"MNU_JIBY_BUSQUEDACLIENTE":"BUSQUEDA CLIENTE Jonathan Borbor"
+			
         }
     }
 }
