963a88f4699ba20b5ce63f74bf03568c075b52e6
Pepe39 <pepecujae@gmail.com>
2021-08-10 12:00:48 -0500

    se agregan menus JSH

--- menu-Semillero.js
+++ menu-Semillero.js
@@ -16,7 +16,20 @@
             "MNU_KOUG_PRUCLS":"Consultar Cliente",
             "MNU_KOUG_PRUBU":"Lista Productos",
             "MNU_EVAL_TONY":"Evaluaci\u00F3n - Tony",
-			"MNU_RSZR":"Menu Roddy Zamora"
+			"MNU_RSZR":"Menu Roddy Zamora",
+			"MNU_JSH_BNVD":"Bienvenido Jose Santiesteban" ,
+			"JSH_ListadoClientes":"Formulario listar clientes",
+			"Jose_Santiesteban_Designer":"Jose Santiesteban",
+			"JSH_MNU_SERVER":"Prueba evento servidor JSH",
+			"JSH_CrearCliente":"Formulario crear Cliente JSH",
+			"JSH_BuscarCliente":"Formulario buscar Cliente",
+			"JSH_NotasForm":"Formulario Notas Jose Santiesteban",
+			"JSH_TiendaForm":"Formulario Tienda Jose Santiesteban",
+			"LstarProdJSH":"Listar Clientes Jose Santiesteban",
+			"Producto JSH":"Formulario producto Jose Santiesteban",
+			"Calificaciones Jose Santiesteban":"Calificaciones Jose Santiesteban",
+			"Calculadora Jose Santiesteban":"Calculadora Jose Santiesteban",
+			"JSH_BusquedaClientes":"Buscar clientes Jose Santiesteban"
         }
     }
 }
