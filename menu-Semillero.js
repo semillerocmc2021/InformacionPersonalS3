1e227dbf9006a85f20c527facef3776f1dd187ce
Marcos <marcos_israel1991@hotmail.com>
2021-08-10 12:33:04 -0500

    menu limpio

--- menu-Semillero.js
+++ menu-Semillero.js
@@ -1,13 +1,13 @@
 {
     "COMMONS": {
         "MENU": {
-            
+            "MNU_SMCMC":"Formulario Semillero",
+			
             "MNU_KOU_BUSCLI":"Buscar Cliente Kevin Ulquiango",
             "MNU_KOU_IMPRIMIR":"Query Cliente Kevin Ulquiango",
             "MNU_KUG_EVP":"Prueba Final Kevin Ulquiango", 
             "MNU_KOU_CREATCLIE":"Crear Cliente Kevin Ulquiango",
             "MNU_KUG_EVAL":"Evaluacion2 Kevin Ulquiango",
-            "MNU_SMCMC":"Formulario Semillero",
             "MNU_KUG_CALC":"Calculadora Kevin Ulquiango",
             "MNU_KUG_PROM":"Calificaciones Kevin Ulquiango",
             "MNU_KOU_BVDA":"Bienvenido Kevin Ulquiango",
@@ -16,10 +16,8 @@
             "MNU_KOUG_PRUCLS":"Consultar Cliente",
             "MNU_KOUG_PRUBU":"Lista Productos",
             "MNU_EVAL_TONY":"Evaluaci\u00F3n - Tony",
-<<<<<<< HEAD
-<<<<<<< HEAD
-			"MNU_RSZR_BVDA":"Menu Roddy Zamora"
-=======
+
+			"MNU_RSZR_BVDA":"Menu Roddy Zamora",
 			"MNU_RSZR":"Menu Roddy Zamora",
 			"MNU_EMO_BNVD":"Bienvenido Erik Montesdeoca",
 			"MNU_EMO_CLFCC":"Calificaciones Erik Montesdeoca",
@@ -28,14 +26,12 @@
 			"MNU_EMO_EVAL3":"Evaluacion 3 - Erik Montesdeoca",
 			"MNU_EVAL2":"Evaluacion 2 - Erik Montesdeoca",
 			"MNU_EVAL3":"Evaluacion 3 - Erik Montesdeoca",
-			"MNU_SMCMC":"Semillero",
 			"MNU_EMO_CLTE":"Formulario cliente - Erik Montesdeoca",
 			"ERIK_MONTESDEOCA":"Erik Montesdeoca - Evaluacion Final",
 			"FORM_PROD": "Formulario Producto",
 			"FORM_LPROD": "Listar Productos",
 			"FORM_LCLI": "Busqueda Clientes"
->>>>>>> bb97126d8b2c528ae61605a80198fc75583e9361
-=======
+
 			"MNU_JIBY_BVDA":"Bienvenido Jonathan Borbor",
 			"MNU_JIBY_CALIFICACIONES":"CALIFICACIONES Jonathan Borbor",
 			"MNU_JIBY_CALCULADORA":"CALCULADORA Jonathan Borbor",
@@ -49,12 +45,7 @@
 			"MNU_JIBY_FORMPRODUCTO":"FORMULARIO PRODUCTO Jonathan Borbor",
 			"MNU_JIBY_LISTARPRODUCTOS":"LISTAR PRODUCTO Jonathan Borbor",
 			"MNU_JIBY_BUSQUEDACLIENTE":"BUSQUEDA CLIENTE Jonathan Borbor"
-<<<<<<< HEAD
->>>>>>> a532394b4f55ba168274c28d4c9970de419dabcc
->>>>>>> ba868ec67ff03a3a8c6ec7944f1ce25de13d8f73
-=======
-			
->>>>>>> 118dddff6a8145ac3d8e20862f556736d9553890
+
         }
     }
 }
